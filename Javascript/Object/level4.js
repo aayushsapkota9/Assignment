a57cbfba8894fc97e55cf297209d94bf08713ed4
aayushsapkota9 <120156314+aayushsapkota9@users.noreply.github.com>
2023-06-25 09:26:59 +0545

    sloved percentage

---
+++ Javascript/Object/level4.js
@@ -0,0 +1,14 @@
+const obj = {
+    computer: 42,
+    maths: 42,
+    science: 50
+}
+
+//calculate the percentage he scored in the exams out of total 150 score
+let calcPerc=(obj)=>{
+// return `${(((obj.computer+obj.maths+obj.science)/150)*100).toFixed(2)}%`
+let total=((Object.values(obj).reduce((sum,value)=>{return sum+=value;},0)/150)*100)
+return `${total.toFixed(2)}%`
+
+}
+calcPerc(obj)
