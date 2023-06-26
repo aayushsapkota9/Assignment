906ac6aa16fa9364f3fec68bcb65daf9855dafee
aayushsapkota9 <120156314+aayushsapkota9@users.noreply.github.com>
2023-06-26 17:57:52 +0545

    solved object manipulation code

--- Javascript/Object/level4.js
+++ Javascript/Object/level4.js
@@ -12,3 +12,44 @@ return `${total.toFixed(2)}%`
 
 }
 calcPerc(obj)
+
+
+//install node js 16.16.0
+
+const userDetails={
+    name: "kaylin",
+  maths: 23,
+  science: 35,
+
+  gk:30
+}
+
+// {
+//     userName: 'kaylin',
+//     subjectCodes: ['M','S','G'],
+//     subjectScores: [23,35,30]
+// }
+
+let newDetails={
+name: '',
+subjectCodes: [],
+subjectScores: []
+}
+// let temp=
+newDetails.subjectCodes=[...Object.keys(userDetails).map((item)=>{
+if(item!='name')
+  {
+    return item[0].toUpperCase()
+    
+  }
+}).filter(item=>item)]
+// newDetails.subjectScores
+
+newDetails.name=userDetails.name
+newDetails.subjectScores=[...Object.values(userDetails).filter((item)=>{
+console.log(item)
+if(typeof(item) != 'string'){
+return item
+}})]
+
+console.log(newDetails)
